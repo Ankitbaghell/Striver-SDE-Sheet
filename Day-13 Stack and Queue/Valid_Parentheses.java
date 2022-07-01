449f71b7310ebc4a3ff6ed1e9308b5e00aab733e
Ankit Baghel <93915122+Ankitbaghell@users.noreply.github.com>
2022-07-01 11:39:40 +0530

    day 13

---
+++ Day-13 Stack and Queue/Valid_Parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        // we have to use stack
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                st.push(s.charAt(i));
+            } else {
+                if ((s.charAt(i) == ')' && !st.empty() && st.peek() == '(')
+                        || (s.charAt(i) == ']' && !st.empty() && st.peek() == '[') ||
+                        (s.charAt(i) == '}' && !st.empty() && st.peek() == '{')) {
+                    st.pop();
+                } else
+                    return false;
+            }
+        }
+        if (st.empty())
+            return true;
+
+        return false;
+    }
+}
